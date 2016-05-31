a61793cf74ba241ba77d76558a8573c4621cd500
Will Thomas <wlthomas001@gmail.com>
2016-05-31 14:45:19 -0600

    added splashpage

---
+++ app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+  //
+  //
+  //   alert("i'm working baby");
+  //   var movie = "Jaws";
+  //
+  //   $.get("https://galvanize-cors-proxy.herokuapp.com/http://www.omdbapi.com/?t=" + movie , function(data) {
+  //     console.log(data);
+  //
+  //     var title = data.Title;
+  //     var actors = data.Actors;
+  //     var awards = data.Awards;
+  //     var poster = data.Poster;
+  //     var plot = data.Plot;
+  //
+  //
+  //     $('.input').append("<h1>" + title + "</h1>");
+  //
+  //     $('.input').append("<br><br>");
+  //
+  //     $('.input').append("<h2>" + actors + "</h2>");
+  //
+  //     $('.input').append("<br><br>");
+  //
+  //     $('.input').append("<h3>" + awards + "</h3>");
+  //
+  //     $('.input').append("<br><br>");
+  //
+  //     $('.input').append('<img src="' + poster + '">');
+  //
+  //     $('.input').append("<br><br>");
+  //
+  //     $('.input').append("<h4>" + plot + "</h4>");
+  //
+  //
+  // })
+
+
+  });
